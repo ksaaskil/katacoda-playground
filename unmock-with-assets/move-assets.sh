ecd1936900a1fcca822638b3d3eb33b6c53dcaca
Kimmo Sääskilahti <kimmo@meeshkan.com>
2019-07-24 11:25:42 +0300

    Refactor.

--- unmock-with-assets/move-assets.sh
+++ unmock-with-assets/move-assets.sh
@@ -2,6 +2,10 @@
 
 mkdir -p __unmock__/hello/
 
-while [ ! -f index.yaml ] ; do sleep 1; done; echo "File exists, moving."
+while [ ! -f index.yaml ]
+do sleep 1
+done
+
+echo "File exists, moving."
 
 mv index.yaml __unmock__/hello/
